6a77d9e92fe7bc0f28b93fd7d716713ea1c949bc
Daniyal Zakir <41327126+Daniyalzakir321@users.noreply.github.com>
2020-12-12 23:18:07 +0500

    OnePush Notifications

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import OneSignal from 'react-native-onesignal'; // Import package from node modules
 
 export default function App() {
 
-  // const onesignal = () => {
+// =============================================
 
   useEffect(() => {
     OneSignal.init("",
-    {
-      kOSSettingsKeyAutoPrompt: false,
-      kOSSettingsKeyInAppLaunchURL: false,
-      kOSSettingsKeyInFocusDisplayOption: 2
-    });
-   
-  }, [])
-   
+      {
+        kOSSettingsKeyAutoPrompt: false,
+        kOSSettingsKeyInAppLaunchURL: false,
+        kOSSettingsKeyInFocusDisplayOption: 2
+      });
 
-  // }
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+
+    return () => {
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    }
+
+  }, [])
+
+  const onReceived = notification => {
+    console.log("Notification received: ", notification);
+  }
+
+  const onOpened = openResult => {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  const onIds = device => {
+    console.log('Device info: ', device);
+  }
+
+// ================================================
 
   return (
     <View style={styles.view}>
